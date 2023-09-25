c04b1ac8b0122454248b99a840be4d74989d8cc4
Devdatta Narawade <113494676+devdattanarawade97@users.noreply.github.com>
2023-09-25 11:56:40 +0530

    Create findingCharacterMostCount.sol

---
+++ smart-contracts/contracts/findingCharacterMostCount.sol
@@ -0,0 +1,48 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.0 <0.9.0;
+
+contract  findingCharacterMostCount {
+
+
+    mapping (bytes1=>uint) charCount;
+       mapping (bytes1=>bool) charCheck;
+
+//Removing white spaces from a string
+    function find(string memory givenString) public  returns (uint){
+     
+       bytes memory bytesGivenString=bytes(givenString);
+     
+
+       for(uint i=0;i<bytesGivenString.length;i++){
+            
+           if(charCheck[bytesGivenString[i]]){
+                  charCount[bytesGivenString[i]]=charCount[bytesGivenString[i]]+1;
+
+           }else{
+charCheck[bytesGivenString[i]]=true;
+                charCount[bytesGivenString[i]]=1;
+
+           }
+     
+           
+       }
+          uint count=0;
+         for(uint i=0;i<bytesGivenString.length;i++){
+            
+           if(charCount[bytesGivenString[i]]>count){
+              
+              count=charCount[bytesGivenString[i]];
+
+           }
+     
+           
+       }
+
+       return  count;
+
+
+
+
+    }
+    
+}
